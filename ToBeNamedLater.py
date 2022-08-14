aef5b2c768d9e04a9f695332f4e1f1b86698c8b9
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-14 11:02:08 +0530

    basic structure of match and balls class created

---
+++ ToBeNamedLater.py
@@ -0,0 +1,30 @@
+
+class balls ():
+    over                        = int
+    ball_no                     = int
+    batsman                     = str
+    bowler                      = str
+    non_stkr                    = str
+    xtras                       = {}
+    runs                        = int
+    player_out                  = None
+    fielder                     = None
+    wicket_kind                 = None
+
+class match ():
+    city                        = str
+    dates                       = {}
+    match_type                  = str
+    p_of_match                  = str
+    teams_a                     = str
+    team_b                      = str
+    toss                        = {}
+    umpires                     = {}
+    venue                       = str
+    first_bat                   = str
+    balls                       = {}
+
+
+
+if __name__ == '__main__':
+    print('5')
