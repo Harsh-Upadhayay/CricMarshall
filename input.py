e0fd1cacf49212c391a188ee5ba265c44f4f44d0
Niraj Matere <81708764+nirajmatere@users.noreply.github.com>
2022-09-14 16:54:37 +0530

    updated input.py

--- input.py
+++ input.py
@@ -20,5 +20,23 @@ with sr.Microphone() as source:
         outputGenerator.WakeUp()
     elif(text == "hello Marshall" or text == "hello"):
         outputGenerator.IntroMessage()
-    else:
-        InputParsing.StepOne(text)
+
+    # """
+    while(text != "thank you marshal"):
+        with sr.Microphone() as source:
+            print("say Something")
+            # read the audio data from the default microphone
+            # audio_data = r.record(source, duration = 1)
+            r.adjust_for_ambient_noise(source)
+            audio_data = r.listen(source)
+            # convert speech to text  
+            text = r.recognize_google(audio_data)
+            print(text)
+            if(text == "wake up Marshal" or text == "wakeup Marshal" or text == "wake up Marshall" or text == "wakeup Marshall"):
+              outputGenerator.WakeUp()
+            elif(text == "hello Marshall" or text == "hello"):
+                outputGenerator.IntroMessage()
+            else:
+                InputParsing.StepOne(text)
+
+    # """
