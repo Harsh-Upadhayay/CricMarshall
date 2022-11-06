c2b12769293cf3a72aa25638c34a29897c481a3e
Niraj Matere <81708764+nirajmatere@users.noreply.github.com>
2022-11-06 18:55:58 +0530

    updated
    extract runs/wickets

--- InputParsing.py
+++ InputParsing.py
@@ -9,6 +9,7 @@ import spacy
 from spacy import displacy
 from spacy import tokenizer
 import calendar
+qp = QueryProcessor()
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -34,6 +35,7 @@ def formatDate(dt):
     return '-'.join(dt)
 
 InputProperNouns = []
+getRequired = []
 def ProperNounExtractor(text):
     sentences = nltk.sent_tokenize(text)
     for sentence in sentences:
@@ -42,17 +44,21 @@ def ProperNounExtractor(text):
         tagged = nltk.pos_tag(words)
         # print(tagged)
         for (word, tag) in tagged:
-            # if tag == 'NNP' or tag == 'NNPS' or tag == 'NN' or tag == 'NNS': 
-            #     print(word)
-            if tag == 'NNP': 
+            # if tag == 'NNP' or tag == 'NNPS' or tag == 'NN' or tag == 'NNS':
+            #     InputProperNouns.append(word) 
+            # #     print(word)
+            if tag == 'NNS': 
                 InputProperNouns.append(word)
-                # print(word)
-        
-        # for(word,tag) in tagged:
-        #     if tag == 'CD':
-        #         print(word)
-    
+            #     # print(word)
+    required = ['runs', 'run', 'wickets', 'wicket']
+
+    for i in required:
+        for j in InputProperNouns:
+            if i == j:
+                getRequired.append(i)
+                break
     # print(InputProperNouns)
+    print(getRequired)
 
 # Inputdates = []
 # def dateExtractor(text):
@@ -87,8 +93,8 @@ InputEvent = [] # list of input events: world cup, asia cup etc
 InputDates = [] # list of input dates
 
 def tag_entities(NER):
-    teams = ['Pakistan', 'Middlesex', 'South Africa', 'Zimbabwe', 'Gibraltar', 'China', 'Sylhet Super Stars', 'Kuwait', 'Cumilla Warriors', 'Rwanda', 'Warwickshire', 'Tanzania', 'Loughborough Lightning', 'Royal Challengers Bangalore', 'Khulna Tigers', 'Mali', 'Warriors', 'Kent', 'Panama', 'Cayman Islands', 'Argentina', 'Pune Warriors', 'Sydney Sixers', 'Romania', 'Botswana', 'Spirit', 'Nigeria', 'Guyana', 'Italy', 'Cape Town Blitz', 'Dhaka Dynamites', 'Sapphires', 'Perth Scorchers', 'St Lucia Stars', 'Sylhet Sixers', 'Otago', 'Trinidad & Tobago Red Steel', 'Dhaka Gladiators', 'West Indies', 'Trent Rockets', 'Norway', 'New South Wales', 'Trailblazers', 'Greece', 'Sierra Leone', 'Victoria', 'United States of America', 'Lightning', 'London Spirit', 'Munster Reds', 'Queensland', 'Hungary', 'Colombo Stars', 'Bhutan', 'Barisal Burners', 'Lesotho', 'Trinbago Knight Riders', 'England', 'Peshawar Zalmi', 'Finland', 'Birmingham Phoenix', 'Canterbury', 'St Helena', 'Gujarat Lions', 'Qatar', 'Japan', 'Mumbai Indians', 'Belize', 'Samoa', 'Kandy Warriors', 'Auckland', 'Thailand', 'Lancashire', 'Welsh Fire', 'Israel', 'Chattogram Challengers', 'Khulna Royal Bengals', 'Paarl Rocks', 'Delhi Capitals', 'Barmy Army', 'Fortune Barishal', 'Leinster Lightning', 'Seychelles', 'Comilla Victorians', 'Philippines', 'Khulna Titans', 'Barisal Bulls', 'Northern Districts', 'Northern Diamonds', 'Quetta Gladiators', 'Thunder', 'Impi', 'Lahore Qalandars', 'Isle of Man', 'Northern Superchargers', 'Nottinghamshire', 'Kandy Tuskers', 'Kochi Tuskers Kerala', 'Rajasthan Royals', 'Galle Gladiators', 'Rajshahi Royals', 'Manchester Originals', 'Jamaica', 'Ghana', 'Falcons', 'Oman', 'South East Stars', 'Bahamas', 'Asia XI', 'Africa XI', 'Gambia', 'Croatia', 'Balkh Legends', 'Scotland', 'Rangpur Riders', 'Delhi Daredevils', 'Slovenia', 'Surrey', 'Uganda', 'Knights', 'France', 'Malta', 'Dambulla Giants', 'Oval Invincibles', 'Luxembourg', 'Eswatini', 'Jaffna Kings', 'Hobart Hurricanes', 'Rising Pune Supergiant', 'Bahrain', 'Tornadoes', 'Western Storm', 'Trinidad and Tobago', 'India', 'Rangpur Rangers', 'Islamabad United', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Belgium', 'Jaffna Stallions', 'Australia', 'Nepal', 'Nangarhar Leopards', 'Nelson Mandela Bay Giants', 'Estonia', 'Guyana Amazon Warriors', 'Cameroon', 'Saudi Arabia', 'Malawi', 'Chittagong Kings', 'St Kitts and Nevis Patriots', 'Ireland', 'Gujarat Titans', 'Chittagong Vikings', 'Netherlands', 'Wellington', 'Colombo Kings', 'Kabul Zwanan', 'Lions', 'Indonesia', 'Birmingham Bears', 'Singapore', 'Hong Kong', 'Boland', 'Western Australia', 'Southern Vipers', 'Myanmar', 'Minister Group Dhaka', 'Southern Brave', 'Leicestershire', 'Windward Islands', 'Duronto Rajshahi', 'Sussex', 'Sydney Thunder', 'Malaysia', 'Lucknow Super Giants', 'Hampshire', 'Kenya', 'Swaziland', 'Leeward Islands', 'United Arab Emirates', 'Punjab Kings', 'Portugal', 'Sunrisers', 'Zambia', 'Derbyshire', 'Mozambique', 'Austria', 'Durham', 'Czech Republic', 'Sylhet Thunder', 'Central Districts', 'Antigua Hawksbills', 'Barbados Tridents', 'Glamorgan', 'Central Sparks', 'Barbados', 'Papua New Guinea', 'Brisbane Heat', 'Somerset', 'Canada', 'Warriors (FairBreak)', 'Rising Pune Supergiants', 'ICC World XI', 'Suriname', 'Surrey Stars', 'Iran', 'Namibia', 'Guernsey', 'Tshwane Spartans', 'North West', 'Cape Cobras', 'Adelaide Strikers', 'Northamptonshire', 'Lancashire Thunder', 'Chennai Super Kings', 'Jersey', 'Sweden', 'Bermuda', 'Supernovas', 'Velocity', 'Kandahar Knights', 'Vanuatu', 'Durban Heat', 'Afghanistan', 'Rajshahi Kings', 'Sylhet Royals', 'Tasmania', 'Sri Lanka', 'Titans', 'Melbourne Renegades', 'Germany', 'Yorkshire', 'Denmark', 'St Lucia Zouks', 'St Lucia Kings', 'New Zealand', 'Melbourne Stars', 'Cyprus', 'Serbia', 'Karachi Kings', 'Western Province', 'Barbados Royals', 'Yorkshire Diamonds', 'Brazil', 'Multan Sultans', 'Essex', 'North-West Warriors', 'Turkey', 'Paktia Panthers', 'Spain', 'Bulgaria', 'Northern Knights', 'Deccan Chargers', 'Dhaka Platoon', 'Jozi Stars', 'Bangladesh', 'Fiji', 'Switzerland', 'Jamaica Tallawahs', 'South Australia', 'Sunrisers Hyderabad', 'Sylhet Sunrisers', 'Dambulla Viiking', 'Dolphins', 'Maldives', 'South Korea', 'Worcestershire', 'Cook Islands', 'Gloucestershire']
-
+    # teams = ['Pakistan', 'Middlesex', 'South Africa', 'Zimbabwe', 'Gibraltar', 'China', 'Sylhet Super Stars', 'Kuwait', 'Cumilla Warriors', 'Rwanda', 'Warwickshire', 'Tanzania', 'Loughborough Lightning', 'Royal Challengers Bangalore', 'Khulna Tigers', 'Mali', 'Warriors', 'Kent', 'Panama', 'Cayman Islands', 'Argentina', 'Pune Warriors', 'Sydney Sixers', 'Romania', 'Botswana', 'Spirit', 'Nigeria', 'Guyana', 'Italy', 'Cape Town Blitz', 'Dhaka Dynamites', 'Sapphires', 'Perth Scorchers', 'St Lucia Stars', 'Sylhet Sixers', 'Otago', 'Trinidad & Tobago Red Steel', 'Dhaka Gladiators', 'West Indies', 'Trent Rockets', 'Norway', 'New South Wales', 'Trailblazers', 'Greece', 'Sierra Leone', 'Victoria', 'United States of America', 'Lightning', 'London Spirit', 'Munster Reds', 'Queensland', 'Hungary', 'Colombo Stars', 'Bhutan', 'Barisal Burners', 'Lesotho', 'Trinbago Knight Riders', 'England', 'Peshawar Zalmi', 'Finland', 'Birmingham Phoenix', 'Canterbury', 'St Helena', 'Gujarat Lions', 'Qatar', 'Japan', 'Mumbai Indians', 'Belize', 'Samoa', 'Kandy Warriors', 'Auckland', 'Thailand', 'Lancashire', 'Welsh Fire', 'Israel', 'Chattogram Challengers', 'Khulna Royal Bengals', 'Paarl Rocks', 'Delhi Capitals', 'Barmy Army', 'Fortune Barishal', 'Leinster Lightning', 'Seychelles', 'Comilla Victorians', 'Philippines', 'Khulna Titans', 'Barisal Bulls', 'Northern Districts', 'Northern Diamonds', 'Quetta Gladiators', 'Thunder', 'Impi', 'Lahore Qalandars', 'Isle of Man', 'Northern Superchargers', 'Nottinghamshire', 'Kandy Tuskers', 'Kochi Tuskers Kerala', 'Rajasthan Royals', 'Galle Gladiators', 'Rajshahi Royals', 'Manchester Originals', 'Jamaica', 'Ghana', 'Falcons', 'Oman', 'South East Stars', 'Bahamas', 'Asia XI', 'Africa XI', 'Gambia', 'Croatia', 'Balkh Legends', 'Scotland', 'Rangpur Riders', 'Delhi Daredevils', 'Slovenia', 'Surrey', 'Uganda', 'Knights', 'France', 'Malta', 'Dambulla Giants', 'Oval Invincibles', 'Luxembourg', 'Eswatini', 'Jaffna Kings', 'Hobart Hurricanes', 'Rising Pune Supergiant', 'Bahrain', 'Tornadoes', 'Western Storm', 'Trinidad and Tobago', 'India', 'Rangpur Rangers', 'Islamabad United', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Belgium', 'Jaffna Stallions', 'Australia', 'Nepal', 'Nangarhar Leopards', 'Nelson Mandela Bay Giants', 'Estonia', 'Guyana Amazon Warriors', 'Cameroon', 'Saudi Arabia', 'Malawi', 'Chittagong Kings', 'St Kitts and Nevis Patriots', 'Ireland', 'Gujarat Titans', 'Chittagong Vikings', 'Netherlands', 'Wellington', 'Colombo Kings', 'Kabul Zwanan', 'Lions', 'Indonesia', 'Birmingham Bears', 'Singapore', 'Hong Kong', 'Boland', 'Western Australia', 'Southern Vipers', 'Myanmar', 'Minister Group Dhaka', 'Southern Brave', 'Leicestershire', 'Windward Islands', 'Duronto Rajshahi', 'Sussex', 'Sydney Thunder', 'Malaysia', 'Lucknow Super Giants', 'Hampshire', 'Kenya', 'Swaziland', 'Leeward Islands', 'United Arab Emirates', 'Punjab Kings', 'Portugal', 'Sunrisers', 'Zambia', 'Derbyshire', 'Mozambique', 'Austria', 'Durham', 'Czech Republic', 'Sylhet Thunder', 'Central Districts', 'Antigua Hawksbills', 'Barbados Tridents', 'Glamorgan', 'Central Sparks', 'Barbados', 'Papua New Guinea', 'Brisbane Heat', 'Somerset', 'Canada', 'Warriors (FairBreak)', 'Rising Pune Supergiants', 'ICC World XI', 'Suriname', 'Surrey Stars', 'Iran', 'Namibia', 'Guernsey', 'Tshwane Spartans', 'North West', 'Cape Cobras', 'Adelaide Strikers', 'Northamptonshire', 'Lancashire Thunder', 'Chennai Super Kings', 'Jersey', 'Sweden', 'Bermuda', 'Supernovas', 'Velocity', 'Kandahar Knights', 'Vanuatu', 'Durban Heat', 'Afghanistan', 'Rajshahi Kings', 'Sylhet Royals', 'Tasmania', 'Sri Lanka', 'Titans', 'Melbourne Renegades', 'Germany', 'Yorkshire', 'Denmark', 'St Lucia Zouks', 'St Lucia Kings', 'New Zealand', 'Melbourne Stars', 'Cyprus', 'Serbia', 'Karachi Kings', 'Western Province', 'Barbados Royals', 'Yorkshire Diamonds', 'Brazil', 'Multan Sultans', 'Essex', 'North-West Warriors', 'Turkey', 'Paktia Panthers', 'Spain', 'Bulgaria', 'Northern Knights', 'Deccan Chargers', 'Dhaka Platoon', 'Jozi Stars', 'Bangladesh', 'Fiji', 'Switzerland', 'Jamaica Tallawahs', 'South Australia', 'Sunrisers Hyderabad', 'Sylhet Sunrisers', 'Dambulla Viiking', 'Dolphins', 'Maldives', 'South Korea', 'Worcestershire', 'Cook Islands', 'Gloucestershire']
+    teams = set(qp.getAllTeams())
     for e in NER:
         # print(e)
         # print(e[0])
@@ -114,7 +120,7 @@ def StepOne(text):
     tokens = nltk.sent_tokenize(text)
     wordToken = nltk.word_tokenize(text)   
     # print(wordToken)
-    # ProperNounExtractor(text)
+    ProperNounExtractor(text)
     # dateExtractor(text)
     entities = NameEntityRecognition(text)
     tagged_entities = tag_entities(entities)
